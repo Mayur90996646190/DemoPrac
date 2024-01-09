d6b02729373b5c4f4115b7cbbd9a8602e72febf3
Mayur Isampelliwar <misampelliwar@gmail.com>
2024-01-09 23:49:16 +0530

    cjdkck

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,5 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
+        System.out.println("Mayur is handsome");
         System.out.println("Mayur");
         System.out.println("From demo class");
         System.out.println("updated master");
