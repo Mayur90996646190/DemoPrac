af57323443218f881116d2d8203375bbc49a507d
Mayur Isampelliwar <misampelliwar@gmail.com>
2024-01-09 22:23:27 +0530

    second commit from mayurbranch

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,5 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
+        System.out.println("Mayur");
         System.out.println("From demo class");
+        System.out.println("From mayur");
     }
 }
