9790b7a394495faae6ac543264719064ee3b4560
Mayur Isampelliwar <misampelliwar@gmail.com>
2024-01-09 23:54:25 +0530

    merge c

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,5 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
+        System.out.println("Mayur ok");
         System.out.println("Mayur");
         System.out.println("From demo class");
         System.out.println("updated master");
